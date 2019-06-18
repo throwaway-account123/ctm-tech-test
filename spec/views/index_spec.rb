6c333557393576c1fe8ee6dcde7a4a0972d60ce7
JeremyEllingham <jeremy@sethindustries.com.au>
2019-06-18 19:35:41 +0100

    Add form.

--- spec/views/index_spec.rb
+++ spec/views/index_spec.rb
@@ -4,8 +4,19 @@ require 'rails_helper'
 
 # Home Page
 feature 'Index page' do
-  scenario 'visit the index page' do
+  it 'visits the index page' do
     visit root_path
     expect(page).to have_content 'Amazing Transaction Viewer by Bean'
+  end
+
+  it 'has instructions' do
+    visit root_path
+    expect(page).to have_content "Enter a transaction description and click 'Submit'"
+  end
+
+  it 'has submission form' do
+    visit root_path
+    expect(page).to have_content 'Enter description:'
+    expect(page).to have_button 'Submit'
   end
 end
