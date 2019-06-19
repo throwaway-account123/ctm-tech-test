d130a37263e7a3c047a5c236c750b976e7e9aa50
JeremyEllingham <jeremy@sethindustries.com.au>
2019-06-19 21:50:03 +0100

    Add tests for prepare_merchant.

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,4 +3,32 @@ require 'rails_helper'
 RSpec.describe Transaction, type: :model do
   it { is_expected.to belong_to(:merchant) }
   it { is_expected.to validate_presence_of(:description) }
+
+  describe '#prepare_merchant' do
+    after :each do
+      Transaction.destroy_all
+      Merchant.destroy_all
+    end
+
+    it 'returns relevant merchant if matching description text' do
+      expected = Merchant.create(name: 'Uber')
+      transaction = Transaction.new(description: 'Uber and some other text')
+      merchant = transaction.prepare_merchant
+      expect(merchant).to eq expected
+    end
+
+    it 'prioritises longer name if multiple merchants matching description text' do
+      Merchant.create(name: 'Uber')
+      expected = Merchant.create(name: 'Uber Eats')
+      transaction = Transaction.new(description: 'some text including uber eats')
+      merchant = transaction.prepare_merchant
+      expect(merchant).to eq expected
+    end
+
+    it 'returns new Merchant called unknown if no merchants matching description text' do
+      transaction = Transaction.new(description: 'Uber')
+      merchant = transaction.prepare_merchant
+      expect(merchant.name).to eq 'unknown'
+    end
+  end
 end
